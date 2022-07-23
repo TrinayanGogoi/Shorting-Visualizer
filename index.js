456e093d6641fa02e4ef5df9adee4c98b7d4ca75
TrinayanGogoi <107055350+TrinayanGogoi@users.noreply.github.com>
2022-07-23 20:52:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,147 @@
+let randomize_array = document.getElementById("randomize_array_btn");
+let sort_btn = document.getElementById("sort_btn");
+let bars_container = document.getElementById("bars_container");
+let select_algo = document.getElementById("algo");
+let speed = document.getElementById("speed");
+let slider = document.getElementById("slider");
+let minRange = 1;
+let maxRange = slider.value;
+let numOfBars = slider.value;
+let heightFactor = 4;
+let speedFactor = 100;
+let unsorted_array = new Array(numOfBars);
+
+//Size Slider
+slider.addEventListener("input", function () {
+  numOfBars = slider.value;
+  maxRange = slider.value;
+  //console.log(numOfBars);
+  bars_container.innerHTML = "";
+  unsorted_array = createRandomArray();
+  renderBars(unsorted_array);
+});
+
+//Speed
+speed.addEventListener("change", (e) => {
+  speedFactor = parseInt(e.target.value);
+});
+
+let algotouse = "";
+
+select_algo.addEventListener("change", function () {
+  algotouse = select_algo.value;
+});
+
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Randomize Array function
+function createRandomArray() {
+  let array = new Array(numOfBars);
+  for (let i = 0; i < numOfBars; i++) {
+    array[i] = randomNum(minRange, maxRange);
+  }
+
+  return array;
+}
+document.addEventListener("DOMContentLoaded", function () {
+  unsorted_array = createRandomArray();
+  renderBars(unsorted_array);
+});
+
+//Displaying Bars
+function renderBars(array) {
+  for (let i = 0; i < numOfBars; i++) {
+    let bar = document.createElement("div");
+    bar.classList.add("bar");
+    bar.style.height = array[i] * heightFactor + "px";
+    bars_container.appendChild(bar);
+  }
+}
+
+//Randomize array Button Function
+randomize_array.addEventListener("click", function () {
+  unsorted_array = createRandomArray();
+  bars_container.innerHTML = "";
+  renderBars(unsorted_array);
+});
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+//Bubble Sort function
+async function bubbleSort(array) {
+  let bars = document.getElementsByClassName("bar");
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        for (let k = 0; k < bars.length; k++) {
+          if (k !== j && k !== j + 1) {
+            bars[k].style.backgroundColor = "aqua";
+          }
+        }
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        bars[j].style.height = array[j] * heightFactor + "px";
+        bars[j].style.backgroundColor = "lightgreen";
+        //bars[j].innerText = array[j];
+        bars[j + 1].style.height = array[j + 1] * heightFactor + "px";
+        bars[j + 1].style.backgroundColor = "lightgreen";
+        //bars[j + 1].innerText = array[j + 1];
+        await sleep(speedFactor);
+      }
+    }
+    await sleep(speedFactor);
+  }
+  return array;
+}
+
+//write insertion sort function
+async function InsertionSort(array) {
+  let bars = document.getElementsByClassName("bar");
+  for (let i = 1; i < array.length; i++) {
+    let key = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      bars[j + 1].style.height = array[j + 1] * heightFactor + "px";
+      bars[j + 1].style.backgroundColor = "red";
+      //bars[j + 1].innerText = array[j + 1];
+      await sleep(speedFactor);
+
+      for (let k = 0; k < bars.length; k++) {
+        if (k != j + 1) {
+          bars[k].style.backgroundColor = "aqua";
+        }
+      }
+      j = j - 1;
+    }
+    array[j + 1] = key;
+    bars[j + 1].style.height = array[j + 1] * heightFactor + "px";
+    bars[j + 1].style.backgroundColor = "lightgreen";
+    //bars[j + 1].innerText = array[j + 1];
+    await sleep(speedFactor);
+  }
+
+  for (let k = 0; k < bars.length; k++) {
+    bars[k].style.backgroundColor = "aqua";
+  }
+  return array;
+}
+
+//Switch Case Selection
+sort_btn.addEventListener("click", function () {
+  switch (algotouse) {
+    case "bubble":
+      bubbleSort(unsorted_array);
+      break;
+    case "insertion":
+      InsertionSort(unsorted_array);
+      break;
+    default:
+      bubbleSort(unsorted_array);
+      break;
+  }
+});
